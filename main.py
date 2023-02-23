2273e3a15b0318267360caebc5d7b5d5727921ff
Nikeshhh <WityazKajit@gmail.com>
2023-02-23 15:19:48 +0300

    Added validation function

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from random import randint
 
 
 def main():
-    pass
+    print("Welcome to the NumberGuesser!")
+
+
+def is_valid(user_input: object) -> bool:  # Checking for valid input value
+    if isinstance(user_input, int):
+        if 1 <= user_input <= 100:
+            return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
