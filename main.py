5ff51935e4877870761ed24de66e782edec21dcf
Nikeshhh <WityazKajit@gmail.com>
2023-02-23 15:35:00 +0300

    Added input messages flexing

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from random import randint
 
 
+INPUT_MESSAGE = "Enter your guess!"
+INPUT_AGAIN_MESSAGE = "Try to enter your guess again!"
+
+
 def main():
+    input_message = INPUT_MESSAGE
     print("Welcome to the NumberGuesser!")
     while True:
-        guess = input("Enter your guess!")
+        guess = input(input_message)
         if is_valid(guess):
             guess = int(guess)
+            input_message = INPUT_MESSAGE
         else:
             print("You probably entered wrong number.")
+            input_message = INPUT_AGAIN_MESSAGE
+            continue
 
 
 def is_valid(user_input: object) -> bool:  # Checking for valid input value
