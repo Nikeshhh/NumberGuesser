84586a5fc0cc958fbd57a488c4b722b826f6dd81
Nikeshhh <WityazKajit@gmail.com>
2023-02-23 15:58:19 +0300

    Refactored architecture, working prototype is done

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 from random import randint
 
 
-INPUT_MESSAGE = "Enter your guess!"
-INPUT_AGAIN_MESSAGE = "Try to enter your guess again!"
+INPUT_MESSAGE = "Enter your guess!\n"  # Standard input message
+INPUT_AGAIN_MESSAGE = "Try to enter your guess again!\n"  # Input message after invalid input
+INPUT_TRY_AGAIN = "Try to guess again!\n"
 
 
-def main():
-    input_message = INPUT_MESSAGE
+def main():  # Main program function
     print("Welcome to the NumberGuesser!")
+
     while True:
-        guess = input(input_message)
-        if is_valid(guess):
-            guess = int(guess)
-            input_message = INPUT_MESSAGE
-        else:
-            print("You probably entered wrong number.")
-            input_message = INPUT_AGAIN_MESSAGE
-            continue
+        user_guess = input_number(input(INPUT_MESSAGE))
+        right_guess = randint(1, 100)
+
+        guess_flag = check_guess(user_guess, right_guess)
+        while guess_flag is False:
+            user_guess = input_number(input(INPUT_TRY_AGAIN))
+            guess_flag = check_guess(user_guess, right_guess)
 
 
-def is_valid(user_input: object) -> bool:  # Checking for valid input value
+def check_guess(user_guess: int, right_guess: int) -> bool:  # Main game function
+    if right_guess < user_guess:
+        print("The right number is below your guess!")
+        return False
+    elif right_guess > user_guess:
+        print("The right number is above your guess!")
+        return False
+    else:
+        print("This is the number, congratulations!")
+        return True
+
+
+def input_number(number: str) -> int:  # Checks number, entered by user and returns it as int when it is valid
+    while is_valid(number) is False:
+        number = input(INPUT_AGAIN_MESSAGE)
+    return int(number)
+
+
+def is_valid(user_input: object) -> int or bool:  # Checking for valid input value
     try:
         user_input = int(user_input)
     except ValueError:
+        print("You probably entered not a number.")
         return False
     if 1 <= user_input <= 100:
-        return True
+        return user_input
     else:
+        print("You probably entered wrong number.")
         return False
 
 
 if __name__ == '__main__':
-    print(is_valid(input()))
+    main()
